f98df50f029e77d728e6949b19a51ddfaf649513
Alexander Phipps <aphipps87@ymail.com>
2025-04-23 23:03:25 -0400

    menu asset update + death plane add
    menu stuff

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,27 +4,39 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
 
-    public CanvasGroup OptionPanel;
+    public CanvasGroup HowToPlayPanel;
+    public CanvasGroup CreditsPanel;
 
     public void PlayGame()
     {
         SceneManager.LoadScene("Hub");
     }
 
-    public void Option()
+    public void HowToPlay()
     {
-        OptionPanel.alpha = 1;
-        OptionPanel.blocksRaycasts = true;
+        HowToPlayPanel.alpha = 1;
+        HowToPlayPanel.blocksRaycasts = true;
     }
     public void Back()
     {
-        OptionPanel.alpha = 0;
-        OptionPanel.blocksRaycasts = false;
+        HowToPlayPanel.alpha = 0;
+        HowToPlayPanel.blocksRaycasts = false;
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        CreditsPanel.alpha = 1;
+        CreditsPanel.blocksRaycasts = true;
+    }
+    public void CreditsBack()
+    {
+        CreditsPanel.alpha = 0;
+        CreditsPanel.blocksRaycasts = false;
     }
 
 }
